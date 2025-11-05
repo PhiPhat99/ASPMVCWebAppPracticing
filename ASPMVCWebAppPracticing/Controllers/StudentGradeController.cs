a5f7b4623d8f65cddf68a556986760347e7e6930
LAPTOP-5QSR3R04\HP <ppman2702@gmail.com>
2025-11-05 23:58:22 +0700

    Add project files.

---
+++ ASPMVCWebAppPracticing/Controllers/StudentGradeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace ASPMVCWebAppPracticing.Controllers
+{
+    public class StudentGradeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
