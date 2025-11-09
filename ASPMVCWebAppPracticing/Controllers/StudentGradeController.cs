ca91a169474c7413d91a5c8be09c7e842bdf5a90
LAPTOP-5QSR3R04\HP <ppman2702@gmail.com>
2025-11-10 00:09:13 +0700

    2nd Update Commit (รู้จักกับ Model - Entity Framework Core).

--- ASPMVCWebAppPracticing/Controllers/StudentGradeController.cs
+++ ASPMVCWebAppPracticing/Controllers/StudentGradeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ASPMVCWebAppPracticing.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
 namespace ASPMVCWebAppPracticing.Controllers
@@ -7,7 +8,27 @@ namespace ASPMVCWebAppPracticing.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            Students s1 = new Students();   // Standard Object Creation
+            s1.Id = 68050808;
+            s1.Name = "Alice Lee";
+            s1.Scores = 88;
+
+            var s2 = new Students();    //Type Interence Objects Creation
+            s2.Id = 68050909;
+            s2.Name = "Bob Scarletta";
+            s2.Scores = 50;
+
+            Students s3 = new();    // Target Typed New Expression Object Creation
+            s3.Id = 68051001;
+            s3.Name = "Charlie Sunday";
+            s3.Scores = 45;
+
+            List<Students> AllStudentsList = new List<Students>();
+            AllStudentsList.Add(s1);
+            AllStudentsList.Add(s2);
+            AllStudentsList.Add(s3);
+
+            return View(AllStudentsList);
         }
 
         public IActionResult Create()
---
+++ ASPMVCWebAppPracticing/Models/Students.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ASPMVCWebAppPracticing.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int Scores { get; set; }
+
+    }
+}
