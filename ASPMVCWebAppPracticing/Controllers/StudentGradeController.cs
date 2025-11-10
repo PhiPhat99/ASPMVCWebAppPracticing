402b76c00c13b14164218cf429815225c006115c
LAPTOP-5QSR3R04\HP <ppman2702@gmail.com>
2025-11-11 00:01:59 +0700

    3rd Update Commit (Application DbContext - บันทึกข้อมูล)

--- ASPMVCWebAppPracticing/Controllers/StudentGradeController.cs
+++ ASPMVCWebAppPracticing/Controllers/StudentGradeController.cs
@@ -1,4 +1,5 @@
-﻿using ASPMVCWebAppPracticing.Models;
+﻿using ASPMVCWebAppPracticing.Data;
+using ASPMVCWebAppPracticing.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Reflection.Metadata.Ecma335;
 
@@ -6,22 +7,28 @@ namespace ASPMVCWebAppPracticing.Controllers
 {
     public class StudentGradeController : Controller
     {
+        private readonly ApplicationDBContext _db;
+        public StudentGradeController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             Students s1 = new Students();   // Standard Object Creation
             s1.Id = 68050808;
             s1.Name = "Alice Lee";
-            s1.Scores = 88;
+            s1.Score = 88;
 
             var s2 = new Students();    //Type Interence Objects Creation
             s2.Id = 68050909;
             s2.Name = "Bob Scarletta";
-            s2.Scores = 50;
+            s2.Score = 50;
 
             Students s3 = new();    // Target Typed New Expression Object Creation
             s3.Id = 68051001;
             s3.Name = "Charlie Sunday";
-            s3.Scores = 45;
+            s3.Score = 45;
 
             List<Students> AllStudentsList = new List<Students>();
             AllStudentsList.Add(s1);
@@ -29,11 +36,24 @@ namespace ASPMVCWebAppPracticing.Controllers
             AllStudentsList.Add(s3);
 
             return View(AllStudentsList);
+
+            //return View();
         }
 
+        // Get Method
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Students obj)
+        {
+            _db.Students.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
--- ASPMVCWebAppPracticing/Models/Students.cs
+++ ASPMVCWebAppPracticing/Models/Students.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ASPMVCWebAppPracticing.Models
 {
@@ -7,8 +8,11 @@ namespace ASPMVCWebAppPracticing.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("ชื่อนักศึกษา")]
         public string Name { get; set; }
-        public int Scores { get; set; }
+        [DisplayName("คะแนนทั้งหมด")]
+        [Range(0, 100)] 
+        public int Score { get; set; }
 
     }
 }
